279682abb6c3e46b03d371921dc898ef1895e236
Farhan <164605246+FarhanAhmad9624@users.noreply.github.com>
2025-02-16 17:49:08 +0530

    connecting with the aws

--- app.py
+++ app.py
@@ -9,17 +9,17 @@ app = Flask(__name__)
 app.secret_key = "6129097fee5199dfa20d2ac8d06ba06ec3ad49c2c3a725ed18ab605e122f4d27028df18652e043e855e08f73119797e7c196b9a4141c9cb4da25d28e70f1b59c"
 
 
-# app.config['MYSQL_HOST'] = "skyler.cj44qsu6gnc1.eu-north-1.rds.amazonaws.com"
-# app.config['MYSQL_PORT'] = 3306
-# app.config["MYSQL_USER"] = "admin"       
-# app.config["MYSQL_PASSWORD"] = "68e30d55846e57ec3ac9be24a9a74bd823933782"
-# app.config["MYSQL_DB"] = "maria"
+app.config['MYSQL_HOST'] = "skyler.cj44qsu6gnc1.eu-north-1.rds.amazonaws.com"
+app.config['MYSQL_PORT'] = 3306
+app.config["MYSQL_USER"] = "admin"       
+app.config["MYSQL_PASSWORD"] = "68e30d55846e57ec3ac9be24a9a74bd823933782"
+app.config["MYSQL_DB"] = "maria"
 
 
-app.config['MYSQL_HOST'] = "localhost"
-app.config["MYSQL_USER"] = "root"       
-app.config["MYSQL_PASSWORD"] = "9624"
-app.config["MYSQL_DB"] = "tulsiyandb"
+# app.config['MYSQL_HOST'] = "localhost"
+# app.config["MYSQL_USER"] = "root"       
+# app.config["MYSQL_PASSWORD"] = "9624"
+# app.config["MYSQL_DB"] = "tulsiyandb"
 
 mysql = MySQL(app)
 
