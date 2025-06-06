4f35193fe935ad45f5102c5133b2e6902b6c839b
Farhan <164605246+FarhanAhmad9624@users.noreply.github.com>
2025-06-06 13:06:34 +0530

    image reader for edit page

---
+++ static/scripts/image_overview.js
@@ -0,0 +1,28 @@
+function imageOverview(){
+    let files = Array.from(document.querySelectorAll('.choose-image'));
+
+    files.forEach(element => {
+        element.addEventListener('change', event=> {
+            console.log('event listener triggered')
+            const files = element.files[0];
+
+            console.log(element.files)
+            console.log(files.type)
+            if(files && files.type.startsWith("image/"))
+            {
+                console.log('file uploaded');
+                console.log(element.previousElementSibling)
+                const reader = new FileReader();
+
+                reader.onload = e=>{
+                    element.previousElementSibling.style.backgroundImage = `url(${e.target.result})`;
+                }
+
+                reader.readAsDataURL(files);
+            }
+        })
+    })
+}
+
+
+imageOverview()
