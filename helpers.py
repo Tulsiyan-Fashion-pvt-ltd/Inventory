66c7e55bdb1e5643229b1a9724ef86e35648bde6
Farhan <164605246+FarhanAhmad9624@users.noreply.github.com>
2025-10-13 23:37:33 +0530

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -14,5 +14,5 @@ def compress_main_image(file) -> bytes:
     img = Image.open(io.BytesIO(file))   # making the file binary object treating as file in the memory
     return_file = io.BytesIO()
     img.thumbnail((800, 800))
-    img.save(return_file, format='JPEG', quality=85, optimize=True)
+    img.save(return_file, format='JPEG', quality=90, optimize=True)
     return return_file.getvalue()
