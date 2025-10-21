3ef1be1a8e139cff19a8959e35e42bd13b1a943e
Farhan <164605246+FarhanAhmad9624@users.noreply.github.com>
2025-10-22 00:15:27 +0530

    Update image compression functions to ensure RGB conversion for consistency

--- helpers.py
+++ helpers.py
@@ -3,7 +3,7 @@ import io
 
 def compress_image(file) -> bytes:
     print('running compressiong')
-    img = Image.open(io.BytesIO(file))   # making the file binary object treating as file in the memory
+    img = Image.open(io.BytesIO(file)).convert('RGB')   # making the file binary object treating as file in the memory
     return_file = io.BytesIO()
     img.thumbnail((420, 560))
     img.save(return_file, format='JPEG', quality=90, optimize=True)
@@ -13,7 +13,7 @@ def compress_image(file) -> bytes:
 
 def compress_main_image(file) -> bytes:
     print('running compressiong')
-    img = Image.open(io.BytesIO(file))   # making the file binary object treating as file in the memory
+    img = Image.open(io.BytesIO(file)).convert('RGB')    # making the file binary object treating as file in the memory
     return_file = io.BytesIO()
     img.thumbnail((800, 800))
     img.save(return_file, format='JPEG', quality=90, optimize=True)
