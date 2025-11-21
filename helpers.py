3ddc9a087564e1522a8695fbf16028c578404f3a
Farhan <164605246+FarhanAhmad9624@users.noreply.github.com>
2025-11-21 15:17:13 +0530

    quality adjustments

--- helpers.py
+++ helpers.py
@@ -6,7 +6,7 @@ def compress_image(file) -> bytes:
     img = Image.open(io.BytesIO(file)).convert('RGB')   # making the file binary object treating as file in the memory
     return_file = io.BytesIO()
     img.thumbnail((420, 560))
-    img.save(return_file, format='JPEG', quality=90, optimize=True)
+    img.save(return_file, format='WEBP', quality=95, optimize=True)
     return return_file.getvalue()
 
 
@@ -16,5 +16,5 @@ def compress_main_image(file) -> bytes:
     img = Image.open(io.BytesIO(file)).convert('RGB')    # making the file binary object treating as file in the memory
     return_file = io.BytesIO()
     img.thumbnail((800, 800))
-    img.save(return_file, format='JPEG', quality=90, optimize=True)
+    img.save(return_file, format='WEBP', quality=100, optimize=True)
     return return_file.getvalue()
