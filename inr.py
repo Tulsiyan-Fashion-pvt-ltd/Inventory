2ae4b6cb855922983e748630a78224eb6cc0cb04
Farhan <164605246+FarhanAhmad9624@users.noreply.github.com>
2026-01-03 08:02:23 +0530

    update inr.py

--- inr.py
+++ inr.py
@@ -6,22 +6,32 @@ class inr:
         self.val = ''
         
     
-    def formate(self):    
-        i = 0
-        for self.num in self.price[::-1]:
-            if i == 3:
-                self.rev_val +=  f",{self.num}"
-            elif i%2!=0 and i>3:
-                self.rev_val += f",{self.num}"
-            else:
-                self.rev_val += self.num
-            i += 1
-            
-        for self.num in self.rev_val[::-1]:
-            self.val += self.num
-        # print(self.val)
-        return '₹'+self.val
+    def formate(self) -> str :
+        # seperate the decimal value 
+        try:
+            self.partition = self.price.split('.')
+            self.price = self.partition[0]
+            self.decimal = self.partition[1]        
+        
+            # adding the comma in the number
+            i = 0
+            for self.num in self.price[::-1]:                
+                if i == 3:
+                    self.rev_val +=  f",{self.num}"
+                elif i%2!=0 and i>3:
+                    self.rev_val += f",{self.num}"
+                else:
+                    self.rev_val += self.num
+                i += 1
+
+            for self.num in self.rev_val[::-1]:
+                self.val += self.num
+            # print(self.val)
+            return self.val+'.'+self.decimal
+        except Exception as e:
+            print(f'exception encountered as \n{e}')
+            return self.price
     
 if __name__ == "__main__":
-    price = inr(200000000).formate()
+    price = inr(200000000.00).formate()
     print(price)
